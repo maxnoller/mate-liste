ff5e2ef6c1a8f64197a3eedeef929653866cffa7
Max Noller <max.noller1@gmail.com>
2020-01-09 16:35:11 +0100

    Updatet finances app to include spending/loading money functionality and test view

--- mate_liste_backend/finances/models.py
+++ mate_liste_backend/finances/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from decimal import *
 
 # Create your models here.
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, db_constraint=False)
     balance = models.DecimalField(max_digits=6, decimal_places=2, help_text="Defines how much money a student has on his account")
+    
+    def chargeAmount(self, amount):
+        self.balance -= Decimal(amount)
+        self.save()
 
+    def loadAmount(self, amount):
+        self.balance += Decimal(amount)
+        self.save()
+        
     def __str__(self):
         return self.user.username
 
--- mate_liste_backend/finances/views.py
+++ mate_liste_backend/finances/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Account
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from kiosk.models import Product
 
-# Create your views here.
+@login_required(redirect_field_name='next', login_url='/login')
+def lowerMoneyByOne(request):
+    Product.buyProduct(request.user, Product.objects.get(name="Club Mate"))
+    return HttpResponse("Maaaate")
