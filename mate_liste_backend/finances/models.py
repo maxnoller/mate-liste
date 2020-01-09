71f70324d5237a408063ce5149bdb965b4636163
Max Noller <max.noller1@gmail.com>
2020-01-09 13:34:03 +0100

    Added student account models and integrated auto create/save when user is created/saved

--- mate_liste_backend/finances/models.py
+++ mate_liste_backend/finances/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, db_constraint=False)
+    balance = models.IntegerField(help_text="Defines how much money a student has on his account")
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_account(sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance, balance=0)
+    
+@receiver(post_save, sender=User)
+def save_user_account(sender, instance, **kwargs):
+    instance.account.save()
