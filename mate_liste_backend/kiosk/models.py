9109496f18e66a21b6a3e9c422b6f94e8b9296b8
Max Noller <max.noller1@gmail.com>
2020-01-09 14:24:27 +0100

    Added Category and Product fields to kiosk app

--- mate_liste_backend/kiosk/models.py
+++ mate_liste_backend/kiosk/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
