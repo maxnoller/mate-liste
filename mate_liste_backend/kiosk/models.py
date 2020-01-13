002288cfe5f723dae50ffe4f51084b0202ceacfd
Max Noller <max.noller1@gmail.com>
2020-01-13 01:45:01 +0100

    Added check if user has enough money to buy product method

--- mate_liste_backend/kiosk/models.py
+++ mate_liste_backend/kiosk/models.py
@@ -14,10 +14,14 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    barcode = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name+": "+self.price.__str__()+"€"
 
     def buyProduct(user, product):
+        if(user.balance < product.price):
+            return False
         account = Account.objects.get(user=user)
         account.chargeAmount(product.price)
+        return True
