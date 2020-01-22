5f07c89dd9f2743827ceca3c606f4dc40fd4e364
Max Noller <max.noller1@gmail.com>
2020-01-22 15:45:24 +0100

    Added user check to buy product view

--- mate_liste_backend/frontend/views.py
+++ mate_liste_backend/frontend/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from kiosk.models import Product
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, "base.html", {})
 
+@login_required(redirect_field_name='next', login_url='/login')
 def scan(request):
     return render(request, "scan.html", {})
 
 def buyProducts(request):
     if not request.GET:
         return HttpResponse("Please pass products as get request")
+    if not request.user:
+        return HttpResponse("Only possible when logged in")
     products = parseProductString(request.GET.get('products'))
     for product in products:
         Product.buyProduct(request.user, product)
