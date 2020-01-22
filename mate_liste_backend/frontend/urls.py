a61fc7e6d708baa72cdba24ffa93dd916d081ca6
Max Noller <max.noller1@gmail.com>
2020-01-22 02:27:07 +0100

    Added scan page & functionality

--- mate_liste_backend/frontend/urls.py
+++ mate_liste_backend/frontend/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register", views.lowerMoneyByOne),
+    path("", views.index, name="index"),
+    path("scan", views.scan, name="scanProduct"),
+    path("getCart", views.getCart, name="getCart"),
+    path("buyProduct", views.buyProduct, name="buyProduct"),
     ]
--- mate_liste_backend/frontend/views.py
+++ mate_liste_backend/frontend/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from kiosk.models import Product
 
 # Create your views here.
+def index(request):
+    return render(request, "base.html", {})
+
+def scan(request):
+    return render(request, "scan.html", {})
+
+def buyProduct(request):
+    return HttpResponse("Nada")
+
+def getCart(request):
+    if not request.GET:
+        return HttpResponse("Please pass products as get request")
+    products = []
+    for productString in request.GET.get("products").split('_'):
+        products.append(Product.objects.get(barcode=productString))
+    return render(request, "cart.html", {"products":products})
+
