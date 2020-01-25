7c95e812c7586789daedf23552401fca4d4346c1
Max Noller <max.noller1@gmail.com>
2020-01-25 16:02:36 +0100

    Updated name of scan url from scanProduct to scan

--- mate_liste_backend/frontend/urls.py
+++ mate_liste_backend/frontend/urls.py
@@ -4,7 +4,7 @@ from .frontend_views import account_views
 from .frontend_views import general_views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("scan", views.scan, name="scanProduct"),
+    path("scan", views.scan, name="scan"),
     path("getCart", views.get_cart_view, name="getCart"),
     path("buyProducts", views.buy_products_view, name="buyProducts"),
     path("logout", account_views.logout_view, name="logout"),
