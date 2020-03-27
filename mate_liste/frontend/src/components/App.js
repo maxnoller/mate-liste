be9fc49dd942da6e6c228f2f30be03ec3f25794b
Aiko <aiko@ubuntu.lenovo>
2020-03-27 19:37:47 +0100

    product test

--- mate_liste/frontend/src/components/App.js
+++ mate_liste/frontend/src/components/App.js
@@ -6,6 +6,7 @@ import {Switch, Route, Link} from "react-router-dom";
 import NavBar from "./Navbar";
 import Login from "./Login";
 import Signup from "./Signup";
+import Product from "./Product"
 
 
 class App extends Component {
@@ -17,8 +18,9 @@ class App extends Component {
           <Switch>
             <Route exact path={"/login/"} component={Login} />
             <Route exact path={"/signup/"} component={Signup} />
-            <Route path={"/"} render={() => <div>Home again</div>} />
+            <Route path={"/"} render={() => <div><Product></Product></div>} />
           </Switch>
+          
         </main>
       </div>
     );
---
+++ mate_liste/frontend/src/components/Product.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { CardMedia } from '@material-ui/core';
+
+
+class Product extends Component{
+
+    render(){
+
+        return (
+
+            <Button>Test</Button>
+
+        );
+
+    }
+
+
+}
+
+export default Product
