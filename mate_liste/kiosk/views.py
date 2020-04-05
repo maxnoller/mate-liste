4efd7799c986395186c403e578f477029ced5ef6
Max Noller <max.noller1@gmail.com>
2020-04-05 21:27:31 +0200

    Added favorite api view

--- mate_liste/kiosk/views.py
+++ mate_liste/kiosk/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 
-from .models import Product
-from .serializers import ProductSerializer, KioskUserSerializer
+from .models import Product, Favorite
+from .serializers import ProductSerializer, KioskUserSerializer, FavoriteSerializer
 
 User = get_user_model()
 
@@ -11,6 +11,10 @@ User = get_user_model()
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class FavoriteViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
 
 class KioskUserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
