682abd01b0bca305debf5e7a7d5b2915c1812a52
maxnoller <max.noller1@gmail.com>
2020-04-12 20:41:26 +0200

    Minor fixed in models to clean up code

--- mate_liste/kiosk/models.py
+++ mate_liste/kiosk/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+user_model = get_user_model()
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -18,15 +20,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name+": "+self.price.__str__()+"€"
 
-    def buyProduct(self ,user ,product):
-        if(user.balance < product.price):
-            return False
-        user.changeBalance(-product.price)
-        return True
-
-user_model = get_user_model()
-
-# Create your models here.
 class Favorite(models.Model):
     user = models.ForeignKey(user_model, on_delete=models.CASCADE, related_name="favorites")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
