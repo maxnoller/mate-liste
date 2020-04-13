bfcd4be96c31c5f2b9e560e121a1d4fb3cf7b27b
Aiko <aiko@ubuntu.lenovo>
2020-04-14 00:19:48 +0200

    kaufen eingefügt

--- mate_liste/frontend/src/components/Product.js
+++ mate_liste/frontend/src/components/Product.js
@@ -1,20 +1,40 @@
 import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
-import { CardMedia, Card, createStyles, CardContent, Typography, Button, CardActions, CardActionArea, IconButton, Divider } from '@material-ui/core';
+import { CardMedia, Card, createStyles, CardContent, Typography, Button, CardActions, CardActionArea, IconButton, Divider, Snackbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import axiosInstance from "../axiosApi";
+import Alert from '@material-ui/lab/Alert';
 
  
 
 class Product extends React.Component{
-    
-    onClick(){
-        alert("test")
-    }
 
     constructor(props){
         super(props);
-        this.state = {name: props.name, image: props.image, price: props.price}
+        this.state = {name: props.name, image: props.image, price: props.price,
+                        user: props.user, id: props.id, open: false,
+                        error: false}
+        this.buyProd = this.buyProd.bind(this)
+    }
+
+    async buyProd(){
+        try {
+
+            let response = await axiosInstance.post("kiosk/transaction/",
+            {
+               user: this.state.user,
+               product: this.state.id
+
+            })
+
+            this.setState({open: true})
+            
+        } catch (error) {
+            
+            this.setState({error: true})
+
+        }
     }
 
     render(){
@@ -34,18 +54,19 @@ class Product extends React.Component{
 
 
             return (
+                <div>
                     <Card className={classes.root}> 
                             <CardMedia 
                             className={classes.media}
-                            title={this.probs.name}
-                            image={this.probs.image}/>
+                            title={this.state.name}
+                            image={this.state.image}/>
                             <CardContent >
                                 <Typography variant="h4">
-                                    {this.probs.name}
+                                    {this.state.name}
                                 </Typography>
                                 
                                 <Typography variant="h5">
-                                    {this.probs.price}€  
+                                    {this.state.price}€  
                                 </Typography>
 
                             </CardContent>
@@ -53,9 +74,10 @@ class Product extends React.Component{
                             <Divider/>
 
                             <CardActions className={classes.button}>
-                                    <Button onClick={onClick}
+                                    <Button
                                     variant="contained"
                                     size="small"
+                                    onClick={this.buyProd}
                                     >Kaufen</Button>
 
                                     <IconButton>
@@ -65,7 +87,22 @@ class Product extends React.Component{
                                     </CardActions>
                             
                     </Card>
+                    <Snackbar open={this.state.open} autoHideDuration={5000}
+                                anchorOrigin={{vertical: "bottom", horizontal: "center"}}
+                                onClose={() => this.setState({open: false})}>
+                        <Alert>
+                            Der Artikel wurde gekauft!
+                        </Alert>
+                    </Snackbar>
 
+                    <Snackbar open={this.state.error} autoHideDuration={5000}
+                                anchorOrigin={{vertical: "bottom", horizontal: "center"}}
+                                onClose={() => this.setState({error: false})}>
+                        <Alert severity="error">
+                            Beim kauf gab es einen Fehler!
+                        </Alert>
+                    </Snackbar>
+                </div>
             );
         }
 
